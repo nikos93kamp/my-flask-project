b69e2b74eba5d41f7894589f2ce91a33ad787011
Nikos Kampouridis <nkampouridis9@gmail.com>
2021-04-29 22:07:29 +0300

    Updated Database Url

--- flaskproject/config.py
+++ flaskproject/config.py
@@ -1,10 +1,9 @@
 import os
-import flask_heroku
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY_2')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
@@ -13,5 +12,3 @@ class Config:
 
     DEBUG = True
     ALLOWED_HOSTS = ['myflaskapp3.herokuapp.com']
-
-    flask_heroku.settings(locals())
