34e5b3495d1d2ee585dcd2a680ac5cb3b9d6726e
Nikos Kampouridis <nkampouridis9@gmail.com>
2021-04-29 22:41:01 +0300

    Fix Database Uri

--- flaskproject/config.py
+++ flaskproject/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY_2')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
